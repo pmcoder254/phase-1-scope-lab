2871b248cd087ee45ea116427daf4af479161197
Paul Muchui <paul.muchungi@gmail.com>
2024-04-02 22:57:03 +0300

    makes necessary chages howeever can not correct one failed test, not sure why

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = 'bob'
+function upperCaseCustomerName() {
+    const upper = customerName.toUpperCase;
+    console.log = upper;
+}
+
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+    
+  }
+  
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'someone';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'someone else'; 
+}
